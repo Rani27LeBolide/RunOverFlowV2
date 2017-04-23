abd88ac5b0fe8f417ad8419082652882cfb4e4e3
toinou hennuyer <toinou hennuyer>
2017-04-23 16:37:10 +0200

    Map canyon
    creation d'une map

--- RunOverFlowv1/Assets/Scripts/loadcartemulti.cs
+++ RunOverFlowv1/Assets/Scripts/loadcartemulti.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class loadcartemulti : MonoBehaviour {
 
     GameObject target;
@@ -11,5 +12,7 @@ public class loadcartemulti : MonoBehaviour {
         target = GameObject.FindGameObjectWithTag("musique");
         DontDestroyOnLoad(target);
         SceneManager.LoadScene("CarteMulti");
+        
+        
     }
 }
